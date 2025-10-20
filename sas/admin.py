b14829cb8a05ec5dd653d26d52f5f76f3b1e27aa
josseycodes1 <adewumijosephine1@gmail.com>
2025-10-20 20:54:04 +0100

    admin changes

--- sas/admin.py
+++ sas/admin.py
@@ -1,18 +1,3 @@
 from django.contrib import admin
 from .models import AnalyzedString
 
-@admin.register(AnalyzedString)
-class AnalyzedStringAdmin(admin.ModelAdmin):
-    list_display = ('value', 'length', 'is_palindrome', 'unique_characters', 'word_count', 'created_at')
-    list_filter = ('is_palindrome', 'length', 'word_count', 'created_at')
-    search_fields = ('value',)
-    readonly_fields = ('id', 'created_at')
-    
-    fieldsets = (
-        ('Basic Information', {
-            'fields': ('id', 'value', 'created_at')
-        }),
-        ('Analysis Results', {
-            'fields': ('length', 'is_palindrome', 'unique_characters', 'word_count', 'character_frequency_map')
-        }),
-    )
